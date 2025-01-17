9d476460fa8bf740ae89092fd981c6ab463dc91e
IvannIvannov <ivann.ivannov26@icloud.com>
2025-01-17 19:51:22 +0200

    search engine complete

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -2,8 +2,21 @@ import { v4 as uuid } from 'uuid';
 import movies from "../movies.js";
 
 export default {
-    getAll(){ 
-        return movies;
+    getAll(filter = {}){ 
+        let result = movies;
+
+        if(filter.search){
+            result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
+        }
+
+        if(filter.genre) {
+            result = result.filter(movie => movie.genre.toLowerCase() === filter.genre);
+        }
+
+        if(filter.year) {
+            result = result.filter(movie => movie.year === filter.year);
+        }
+        return result;
     },
     
     findOne(movieId) {
